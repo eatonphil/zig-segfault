fc423771bf23d69fd8174a83d95b0eadec14b398
Phil Eaton <phil@eatonphil.com>
2023-07-17 16:48:33 +0000

    Indexed loop

--- src/tigerbeetle/client.zig
+++ src/tigerbeetle/client.zig
@@ -546,8 +546,9 @@ pub fn ClientType(comptime StateMachine: type, comptime MessageBus: type) type {
             var allocator = arena.allocator();
             var batch = try std.ArrayList(T).initCapacity(allocator, objects.len);
 
-            for (objects) |object| {
-                batch.appendAssumeCapacity(@field(object, @tagName(name)));
+            var i: u32 = 0;
+            while (i < objects.len) : (i += 1) {
+                batch.appendAssumeCapacity(@field(objects[i], @tagName(name)));
             }
 
             assert(batch.items.len == objects.len);
